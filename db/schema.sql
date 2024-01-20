cca131ab1e7f38f89d111b51c882a80bd8bbf751
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-01-20 14:09:04 -0700

    Update database configuration and schema

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+-- Create the database --
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+
+-- Use the database --
+USE employee_tracker_db;
+
+-- Create the department table --
+CREATE TABLE department (
+    id INT NOT NULL,
+    name VARCHAR(30),
+);
+
+-- Create the role table --
+CREATE TABLE role (
+    id INT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+-- Create the employee table --
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+);
