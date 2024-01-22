16f180c7349c827cdd4720c3f6f9a9d34321e54d
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-01-21 20:36:57 -0700

    Add first 3 query functions

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 // Connect to database
 const db = mysql.createConnection({
@@ -9,3 +10,40 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME
 },
 console.log(`Connected to the ${process.env.DB_NAME} database.`));
+
+// Function to view all departments
+function viewAllRoles() {
+    db.query('SELECT * FROM department', function (err, results) {
+        console.log(results);
+    });
+};
+
+// Function to view all roles
+function viewAllRoles() {
+    db.query('SELECT * FROM role', function (err, results) {
+        console.log(results);
+    });
+};
+
+// Function to view all employees
+function viewAllEmployees() {
+    db.query('SELECT * FROM employee', function (err, results) {
+        console.log(results);
+    });
+};
+
+// Function to add a department
+function addDepartment() {
+    
+};
+
+// Function to add a role
+function addRole() {};
+
+// Function to add an employee
+function addEmployee() {};
+
+// Function to update an employee role
+function updateEmployeeRole() {};
+
+// Quit the application
