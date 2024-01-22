ca457740dac04287482ad36370eeb3e2f2ebe183
Adrian Ortiz <147290741+Agerian@users.noreply.github.com>
2024-01-22 15:25:54 -0700

    Refactor viewAllEmployees function to include additional information

--- index.js
+++ index.js
@@ -32,48 +32,21 @@ function viewAllRoles() {
 
 // Function to view all employees
 function viewAllEmployees() {
-    db.query('SELECT * FROM employee', function (err, employees) {
+    db.query(
+        'SELECT employee.*, CONCAT(role.id, \': \' , role.title) AS role_id, manager.first_name AS manager_first_name, manager.last_name AS manager_last_name FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN employee AS manager ON employee.manager_id = manager.id', function (err, results) {
         if (err) {
             console.error(err);
             menu();
             return;
         }
 
-        // Fetch additional information about roles
-        db.query('SELECT id, title FROM role', function (err, roles) {
-            if (err) {
-                console.error(err);
-                menu();
-                return;
-            }
+        // Log the results
+        console.log(results);
 
-            // Fetch additional information about managers
-            db.query('SELECT id, CONCAT(first_name, " ", last_name) AS manager FROM employee WHERE manager_id IS NOT NULL', function (err, managers) {
-                if (err) {
-                    console.error(err);
-                    menu();
-                    return;
-                }
-
-                // Map roles and managers for easy access
-                const rolesMap = new Map(roles.map(role => [role.id, role.title]));
-                const managersMap = new Map(managers.map(manager => [manager.id, manager.manager_name]));
-
-                // Display results with additional info
-                const formattedEmployees = employees.map(employee => ({
-                    id: employee.id,
-                    first_name: employee.first_name,
-                    last_name: employee.last_name,
-                    role: `${employee.role_id}: ${rolesMap.get(employee.role_id)}`,
-                    manager: employee.manager_id ? `${employee.manager_id}: ${managersMap.get(employee.manager_id)}` : null
-                }));
-
-                console.log(formattedEmployees);
-                menu();
-            });
-        });
+        // Continue to the menu
+        menu();
     });
-}
+};
 
 // Function to add a department
 function addDepartment() {
